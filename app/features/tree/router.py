8cd98b83a6fbebf56219d5637da0c0519c284db0
Koliy82 <sokol070804@mail.ru>
2024-08-09 12:37:02 +0300

    chore: change result to text

--- app/features/tree/router.py
+++ app/features/tree/router.py
@@ -15,11 +15,11 @@ async def root():
     return "tree"
 
 
-@router.get("/text/{user_id}", responses={404: {"description": "Family tree not found"}})
+@router.get("/text/{user_id}")
 async def family_tree(user_id: int, reverse: bool = True):
     brak = get_brak_by_user_id(user_id)
     if not brak:
-        raise HTTPException(status_code=404, detail=f"Brak with user_id={user_id} not found")
+        return PlainTextResponse("Брак не найден")
 
     tree = FamilyTree(user_id)
     tree_lib = tree.to_treelib()
